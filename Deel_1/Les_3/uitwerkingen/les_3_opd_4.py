b7f7635a5e7c37cb81fb5a37d7d2147fb5033c58
christiandavinci <99075858@mydavinci.nl>
2023-09-04 23:25:59 +0200

    Update les_3_opd_4.py

--- Deel_1/Les_3/uitwerkingen/les_3_opd_4.py
+++ Deel_1/Les_3/uitwerkingen/les_3_opd_4.py
@@ -1,4 +1,11 @@
-naam = input("wat is je naam?")
-weer = input("wat is het weer ?")           
+woord1 = input ('voer een woord in voor woord1')
+stad1 = input ('voer een stadnaam in voor stad1')
+naam1 = input ('voer een naam in voor naam1')
+woord2 = input ('voer een woord in voor woord2')
+woord3 = input ('voer een woord in voor woord3')
+woord4 = input ('voer een woord in voor woord4')
+woord5 = input ('voer een woord in voor woord5')
+woord6 = input ('voer een woord in voor woord6')
 
-print("hallo"+ naam + "het is" + weer + "weer")           
+
+print('Het was een' + woord1 + ' dag in' + stad1 + ' . ' +  naam1 + 'liep door de straten, op zoek naar een' + woord2 + ' Plotseling zag' + naam1 + ' een' + woord3 + 'man/vrouw die een' + woord4 + 'verkocht' + naam1 + ' besloot om het te kopen en liep verder Toen' + naam1 + 'bij een'  + woord5 + 'kwam zag hij/zij een groep mensen die' + woord6 + 'test' + naam1 + ' besloot om mee te doen en had de tijd van zijn/haar leven')
