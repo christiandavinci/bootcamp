e8a4fb63a96bb5a7dd250c443f5caa297609c07b
christiandavinci <99075858@mydavinci.nl>
2023-09-04 15:02:38 +0200

    opdrachten

---
+++ les_3_opd_3.py
@@ -0,0 +1,28 @@
+print ("OPDRACHT 1B")
+
+print
+
+appels  = 3.40
+
+druiven = 2.45
+
+bananen = 1.95
+print ("Zoveel kost het fruit bij elkaar:")
+result2 = appels + druiven + bananen 
+
+print (result2)
+
+print ("Nu doen we dat gedeeld door 100.")
+
+d = 100
+
+result3 = result2 / d
+
+print (result3)
+print ("Nu doen we dat keer 9.")
+
+b = 9
+
+result4 = result3 * 9
+
+print (result4)
---
+++ les_3_opd_4.py
@@ -0,0 +1,4 @@
+naam = input("wat is je naam?")
+weer = input("wat is het weer ?")           
+
+print("hallo"+ naam + "het is" + weer + "weer")           
