54254d6a22a52882c2caba6f7d0a5d04cb68b396
christiandavinci <99075858@mydavinci.nl>
2023-09-06 09:05:54 +0200

    Create les_4_opd_2.py

---
+++ Deel_1/Les_4/uitwerkingen/les_4_opd_2.py
@@ -0,0 +1,4 @@
+print(5*2 -3+4/2) # uitkomst:9.0 (volgt de volgorde van wiskundige bewerkingen )
+print(5*2 - 3+4 /2) # uitkomst: 9.0 (volgt dezelfde volgorde van wiskundige bewerkingen ) 
+print((5*2) - (3+4) /2) # uitkomst 6.5 (eerstbinnen de haakjes, dan deling en daarna aftrekking)
+print(((5*2) -(3+4)) / 2) # uitkomst 2.0 (eerst binnen de haakjes, dan aftrekking, en daarna deling )
