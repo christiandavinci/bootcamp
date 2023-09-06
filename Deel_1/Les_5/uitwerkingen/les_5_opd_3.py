56acb69945e6b5f7d67aba4b1d0e68451db12960
christiandavinci <99075858@mydavinci.nl>
2023-09-06 13:28:32 +0200

    Create les_5_opd_3.py

---
+++ Deel_1/Les_5/uitwerkingen/les_5_opd_3.py
@@ -0,0 +1,5 @@
+getal1 = float(input('geef me een cijfer #1 '))
+getal2 = float(input('geef me een cijfer #2 '))
+getal3 = float(input('geef me een cijfer #3 '))
+result = (getal1 + getal2 + getal3) / 3
+print(f'De gemiddelde van de 3 cijfers is {result}')
