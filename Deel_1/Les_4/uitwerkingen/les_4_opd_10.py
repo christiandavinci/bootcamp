a229d522904db86e575c1e4a24d378a42586e655
christiandavinci <99075858@mydavinci.nl>
2023-09-06 09:05:48 +0200

    Create les_4_opd_10.py

---
+++ Deel_1/Les_4/uitwerkingen/les_4_opd_10.py
@@ -0,0 +1,14 @@
+seconden_per_minuut = 60
+minuten_per_uur = 60
+uren_per_dag = 24
+dagen_per_week = 7
+dagen_per_jaar = 365
+
+
+seconden_in_dag = seconden_per_minuut * minuten_per_uur * uren_per_dag
+seconden_in_week = seconden_in_dag * dagen_per_week
+seconden_in_jaar = seconden_in_dag * dagen_per_jaar
+
+print("seconden in een dag:", seconden_in_dag)
+print("seconden in een week:", seconden_in_week)
+print("seconden in jaar", seconden_in_jaar)
