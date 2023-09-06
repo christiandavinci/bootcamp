83697ab3f9b9f822161cb43752c40e15e4a86add
christiandavinci <99075858@mydavinci.nl>
2023-09-06 13:28:48 +0200

    Create les_6_opd_2.py

---
+++ Deel_1/Les_6/uitwerkingen/les_6_opd_2.py
@@ -0,0 +1,10 @@
+a = 3
+b = 7
+if a > b:
+     print(f"varaibele a is het grootste want {a} is groter dan {b}")
+elif b > a: 
+     print(f"varaibele b is het grootst want {b} is groter dan {a}")  
+else:
+     print("varaibele a en b zijn gelijk.")     
+
+
