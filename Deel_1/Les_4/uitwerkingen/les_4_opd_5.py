a65dafd69ded6f63d9865bba6c3d54ffb042cb5e
christiandavinci <99075858@mydavinci.nl>
2023-09-06 09:06:32 +0200

    Create les_4_opd_5.py

---
+++ Deel_1/Les_4/uitwerkingen/les_4_opd_5.py
@@ -0,0 +1,5 @@
+variable_een = 25
+variable_twee = 0
+resutaalt = variable_een / variable_twee
+print(resutaalt)
+# dit zorgt er voor voor een zerodivisioerror omdat je door nul deelt en dat kan niet
