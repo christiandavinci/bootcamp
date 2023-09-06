2c5952f859e5d894607516bb29f243a1958f5346
christiandavinci <99075858@mydavinci.nl>
2023-09-06 09:05:26 +0200

    Create les_3_opd_4.py

---
+++ bootcamp/les_3_opd_4.py
@@ -0,0 +1,12 @@
+Woord1 = input ("wat voor weer is het?")
+stad1 = input ("in welk stad?")
+land1 = input ("in welk land")
+woord2 = input ("wat voor winkels zijn er?")
+woord3 = input ("wat voor eten hebben ze?")
+woord4 = input ("hebben ze een mooie cultuur?")
+woord5 = input ("zijn er mooie meisjes?")
+woord6 = input ("kan je met een voertuig gaan?")
+woord7 = input ("heb je en leuke dag?")
+
+print("")
+# met ctrl + / op een regel verandert deze in commetaar
