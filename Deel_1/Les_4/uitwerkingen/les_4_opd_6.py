3e8f323a455b4a27f48cb7a4ae4122bf6c6a39f3
christiandavinci <99075858@mydavinci.nl>
2023-09-06 09:06:39 +0200

    Create les_4_opd_6.py

---
+++ Deel_1/Les_4/uitwerkingen/les_4_opd_6.py
@@ -0,0 +1,2 @@
+print( ((2*3 /4 + (5 -6/7) *8  ))) # de uitkomst is 34.642857142857146, er was  een fout er ontbreekte een sluitend haakje
+print( ((12*13 /14) + (15 -16) /17) *18 ) # uitkomst 199.5126050420168 geen veranderingen
