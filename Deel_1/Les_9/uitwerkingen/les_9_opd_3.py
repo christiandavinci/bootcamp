50e9fececd69499fb7dadf83daf6217931259b3f
christiandavinci <99075858@mydavinci.nl>
2023-09-08 13:26:30 +0200

    les 9 opdracht 2 en 3

---
+++ Deel_1/Les_9/uitwerkingen/les_9_opd_3.py
@@ -0,0 +1,9 @@
+leeftijd = 18
+snor = 'j'
+diploma = 'j'
+
+if (leeftijd >= 18 and  snor == 'j') or (leeftijd < 18  and diploma == 'j') :
+    print('je bent aangenomen')
+else:
+    print('sorry, je voldoet niet aan de vereisten voor aanname')
+
