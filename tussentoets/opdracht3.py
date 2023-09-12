6f3105dda540e397e5c79cefd1da5bf597fc88f3
christiandavinci <99075858@mydavinci.nl>
2023-09-12 22:05:06 +0200

    fix opdracht 3

--- tussentoets/opdracht3.py
+++ tussentoets/opdracht3.py
@@ -6,17 +6,3 @@ if (leeftijd >= 18):
     print(f'Beste {naam} , je bent 18 of ouder en mag dus alleen autorijden (met rijbewijs althans).')
 else:
     print(f'Beste {naam} , je bent nog geen 18. Alleen autorijden zit er dus niet in :-( .')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
