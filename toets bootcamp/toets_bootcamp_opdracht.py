c2f71fdedb18dc06febddc3aefb5487bc6b6bd47
christiandavinci <99075858@mydavinci.nl>
2023-09-27 14:14:06 +0200

    toets

--- toets bootcamp/toets_bootcamp_opdracht.py
+++ toets bootcamp/toets_bootcamp_opdracht.py
@@ -42,13 +42,13 @@
 
  
 
-# Opdracht 5: 
-# # Schrijf een functie die 3 getallen bij elkaar optelt en zorg ervoor
-# # dat de uitkomst ervan wordt getoond in de print
+# # Opdracht 5: 
+# # # Schrijf een functie die 3 getallen bij elkaar optelt en zorg ervoor
+# # # dat de uitkomst ervan wordt getoond in de print
 # getal1 = 200
 # getal2 = 5
 # getal3 = 12
-# antwoord = som(getal1, getal2, getal3)# of de naam van je eigen functie.
+# antwoord = print(getal1, getal2, getal3)# of de naam van je eigen functie.
 # print(f"De som van {getal1} + {getal2} + {getal3} = {antwoord}")
 
  
